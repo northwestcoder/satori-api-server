9cd52170e7e87acd0a99376cd7029fb34db952fc
Ty Alevizos <ty@satoricyber.com>
2022-09-26 08:15:43 -0700

    refactored

--- apiauth.py
+++ apiauth.py
@@ -1,13 +1,14 @@
 from functools import wraps
 from flask import request, abort
-import client
+
+apikey = "mylongapikey123890sdflkjw45w"
 
 # decorator function
 def require_apikey(view_function):
 	@wraps(view_function)
 	# the new, post-decoration function. Note *args and **kwargs here.
 	def decorated_function(*args, **kwargs):
-		if request.headers.get('x-apikey') and request.headers.get('x-apikey') == client.instance_apikey:
+		if request.headers.get('x-apikey') and request.headers.get('x-apikey') == apikey:
 			return view_function(*args, **kwargs)
 		else:
 			abort(401)
